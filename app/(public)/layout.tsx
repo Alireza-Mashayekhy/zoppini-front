d039a20208b93abb9d826611d88d687b5162ff84
AlirezaMashayekhy <alireza@sheydal.com>
2026-08-20 19:23:40 +0330

    feat: add goftino to site

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import Footer from '@/components/layout/public/footer';
 import Header from '@/components/layout/public/header';
 
@@ -8,8 +10,40 @@ export default function PublicLayout({
 }>) {
   return (
     <>
+      <Script
+        id="goftino-widget"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(){
+              var i="7Y752F",
+                  a=window,
+                  d=document;
+
+              function g(){
+                var g=d.createElement("script"),
+                    s="https://www.goftino.com/widget/"+i,
+                    l=localStorage.getItem("goftino_"+i);
+
+                g.async=!0;
+                g.src=l?s+"?o="+l:s;
+                d.getElementsByTagName("head")[0].appendChild(g);
+              }
+
+              "complete"===d.readyState
+                ? g()
+                : a.attachEvent
+                  ? a.attachEvent("onload",g)
+                  : a.addEventListener("load",g,!1);
+            }();
+          `,
+        }}
+      />
+
       <Header />
+
       {children}
+
       <Footer />
     </>
   );
