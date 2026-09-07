600ce98ff1e6c014205e6ea56c9067bd9e16dd30
AlirezaMashayekhy <alireza@sheydal.com>
2026-09-07 17:47:53 +0330

    feat(invitation): add invitation page

--- app/(public)/invitation/page.tsx
+++ app/(public)/invitation/page.tsx
@@ -31,6 +31,7 @@ export default function InvitationPage() {
         width={2560}
         height={2049}
         objectFit="cover"
+        loading="eager"
         className="h-[calc(100vh-52px)] w-auto mx-auto hidden sm:block"
       />
       <Image
@@ -39,6 +40,7 @@ export default function InvitationPage() {
         width={2560}
         height={2049}
         objectFit="cover"
+        loading="eager"
         className="w-full h-auto mx-auto sm:hidden"
       />
     </div>
